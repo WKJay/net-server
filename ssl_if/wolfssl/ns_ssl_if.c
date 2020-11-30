7dd97ff737b4c9051349026e462f5eb32caf38cb
WKJay <1931048074@qq.com>
2020-11-30 16:50:38 +0800

    [ADD] add ssl interface directory

---
+++ ssl_if/wolfssl/ns_ssl_if.c
@@ -0,0 +1,17 @@
+/*************************************************
+ Copyright (c) 2020
+ All rights reserved.
+ File name:     ns_ssl_if.c
+ Description:
+ History:
+ 1. Version:
+    Date:       2020-11-30
+    Author:     wangjunjie
+    Modify:
+*************************************************/
+#include <wolfssl/options.h>
+#include <wolfssl/ssl.h>
+
+void *ns_ssl_if_context_create(void) {
+    
+}
